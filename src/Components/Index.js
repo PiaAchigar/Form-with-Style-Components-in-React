52f8ec6df45ea0517c84f8efc7979b8eed597dc4
Maria <achigarpia@gmail>
2021-11-11 12:48:34 -0300

    Index ppal

--- src/Components/Index.js
+++ src/Components/Index.js
@@ -1,20 +1,22 @@
 import React from "react";
+import { Nav, DivContainer, DivQueEs } from "../Elements/StyleIndex";
 
 const Index = () => {
-  return <div>
-      <nav>
-        Bienvenida 
-      </nav>
-      <div>
-          Que es holdup
-      </div>
-      <div>
-          Comienza ahora
-      </div>
-      <footer>
-          redes
-      </footer>
-  </div>;
+  return (
+    <div>
+      <Nav>
+        <p>¡Te damos la Bienvenida al HOLD_UP!</p>
+      </Nav>
+      <DivContainer>
+        <DivQueEs>
+          <p>¿Que es Hold_up?</p>
+        </DivQueEs>
+        <div>Comienza ahora</div>
+      </DivContainer>
+
+      <footer>redes</footer>
+    </div>
+  );
 };
 
 export default Index;
