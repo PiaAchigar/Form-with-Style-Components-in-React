977edf516e84de7ab3d46c6e5ba2a49e16271a7f
Maria <achigarpia@gmail>
2021-11-12 08:07:29 -0300

    StylePrincipal

--- src/Components/Principal.js
+++ src/Components/Principal.js
@@ -1,28 +1,57 @@
 import React from "react";
 import { Banner, Content, DivQueEs } from "../Elements/StylesPrincipal";
-import {Link} from 'react-router-dom'
-import imgFondo from '../Assets/images/bg-index.png'
+import { Link } from "react-router-dom";
+import imgFondo from "../Assets/images/bg-index.png";
 
 const Principal = () => {
   return (
-    
     <section>
-     <Banner>
-       <div>
+      <Banner>
+        <div>
           <img src={imgFondo}></img>
-       </div>
-      <Content>
-        <h1> ¡Participa en<br/> el hold-up!</h1>
-        <p>
-        Explora la plataforma y ayuda a emprendedorxs a resolver retos de su proyecto a través de la inteligencia colectiva
-        </p>
-      </Content> 
-      
-     </Banner>
+        </div>
+        <Content>
+          <h1>
+            {" "}
+            ¡Participa en
+            <br /> el hold-up!
+          </h1>
+          <p>
+            Explora la plataforma y ayuda a emprendedorxs a resolver retos de su
+            proyecto a través de la inteligencia colectiva
+          </p>
+        </Content>
+      </Banner>
+      <div>
+        <div>
+          <p>¿Qué es el HOLD_UP?</p>
+          <p>
+            Plataforma de co-creación entre emprendedores socioambientales y
+            ciudadanos para dar solución a un reto específico de un proyecto.
+          </p>
+        </div>
+        <img></img>
+      </div>
+      <div>
+        <div>
+          <Link to="/form">
+            {" "}
+            <p>Quiero participar como</p>
+          </Link>
 
-     <Link to="/form"> ir al form</Link>
-     </section>
-    
+          <p>emprendedor/a</p>
+        </div>
+        <div>
+          <div>
+            <p>Quiero participar como</p>
+            <p>ciudadano/a</p>
+          </div>
+        </div>
+        <p>
+          Ya tengo <span>cuenta</span>
+        </p>
+      </div>
+    </section>
   );
 };
 
--- src/Elements/StylesPrincipal.js
+++ src/Elements/StylesPrincipal.js
@@ -1,33 +1,36 @@
 import styled from "styled-components";
-import {colores} from './StylesApp'
-import imgFondo from '../Assets/images/bg-index.png'
-
+import { colores } from "./StylesApp";
+import imgFondo from "../Assets/images/bg-index.png";
 
 const Banner = styled.section`
- max-width: 100vw;
-    div{
-        width:100%;
-        img{
-            width:100%;
-        }
+  max-width: 100vw;
+  div {
+    width: 100%;
+    img {
+      width: 100%;
     }
-    `
+  }
+`;
 
 const Content = styled.section`
-    position: absolute;
-    top:17%;
-    left:6vw;
-    width:50vw;
-    color: ${colores.darkblue};
-    h1{
-        font-weight: 800;
-        font-size: 5rem;
-        margin-bottom:0;
-    }
-    p{
-        font-size: 2rem;
-        font-weight: 600;
-    }
-
-`
-export { Banner, Content };
+  position: absolute;
+  top: 17%;
+  left: 6vw;
+  width: 50vw;
+  color: ${colores.darkblue};
+  h1 {
+    font-weight: 800;
+    font-size: 5rem;
+    margin-bottom: 0;
+  }
+  p {
+    font-size: 2rem;
+    font-weight: 600;
+  }
+`;
+const DivCont = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: space-between;
+`;
+export { Banner, Content,DivCont };
