247c56017ffea1fd1cbbd44069f31b1911625a82
Maria <achigarpia@gmail>
2021-11-19 19:50:13 -0300

    login

--- src/App.js
+++ src/App.js
@@ -1,40 +1,31 @@
 import React from "react";
 
-
-import {DivApp } from "./Elements/StylesApp";
+import { DivApp } from "./Elements/StylesApp";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Formulario from "./Components/Form";
 import Index from "./Components/Principal";
-import Retos from './Components/retos/Retos'
- 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from 'react-router-dom'
- 
+import Retos from "./Components/retos/Retos";
+import Foro from "./Components/Foro/Foro";
+import Login from "./Components/Login";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 function App() {
   return (
-
     <Router>
       <DivApp>
-      
-      <Header></Header>
-      <Routes>  
-          
-         
-          <Route path='/retos' element={<Retos />} />
-          <Route path='/form' element={<Formulario />}/>
-          <Route path='/' element={<Index />}/>
-          
-      </Routes>
-      <Footer></Footer>
+        <Header></Header>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/foro" element={<Foro />} />
+          <Route path="/retos" element={<Retos />} />
+          <Route path="/form" element={<Formulario />} />
+          <Route path="/" element={<Index />} />
+        </Routes>
+        <Footer></Footer>
       </DivApp>
-
     </Router>
-   
   );
 }
 export default App;
---
+++ src/Components/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ForoF } from "../Elements/StylesPrincipal";
+
+const Login = () => {
+  return (
+    <ForoF>
+      <h1>En construccion...</h1>
+    </ForoF>
+  );
+};
+
+export default Login;
