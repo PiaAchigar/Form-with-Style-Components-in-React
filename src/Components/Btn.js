eadeb90ef761e996b456c91f681b92579a591f40
Maria <achigarpia@gmail>
2021-11-25 12:49:38 -0300

    styleBtn

--- src/Components/Btn.js
+++ src/Components/Btn.js
@@ -1,54 +1,5 @@
 import React from "react";
-import styled from "styled-components";
-
-const DivCiudadano = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  section {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 26rem;
-    height: 15rem;
-    margin: 0.5rem;
-    border-radius: 30rem;
-    background-color: ${(props) => props.Bgcolor};
-    p {
-      margin: 0;
-      color: white;
-      font-size: 200%;
-      font-weight: 600;
-    }
-    h2 {
-      margin: 0;
-      color: ${(props) => props.Color};
-      font-size: 250%;
-      font-weight: 700;
-    }
-  }
-  div {
-    text-align: center;
-    padding: 1rem;
-    color: white;
-    font-size: 150%;
-    font-weight: 600;
-    width: 25rem;
-    height: 11rem;
-    margin: 0.5rem;
-    border-radius: 5rem;
-    background-color: ${(props) => props.Bgcolor};
-    z-index: 99;
-    display: none;
-  }
-  &:hover {
-    div {
-      display: block;
-      transform: translateY(10px);
-    }
-  }
-`;
+import { DivCiudadano } from "../Elements/Stylebtn";
 
 const Btn = ({ parrafo, persona, descripcion, bgcolor, color }) => {
   return (
